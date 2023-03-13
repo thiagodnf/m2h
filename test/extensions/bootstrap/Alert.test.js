93b373c26d73fdffb0f35e214c6e7f67054a461a
Thiago Ferreira <thiagodnf@gmail.com>
2023-03-13 13:55:06 -0400

    added more test cases

--- test/extensions/bootstrap/Alert.test.js
+++ test/extensions/bootstrap/Alert.test.js
@@ -6,11 +6,46 @@ beforeEach(() => {
     m2h = new M2H();
 });
 
-test("should return correct alert", async () => {
+test("should return correct primary alert", async () => {
 
     let md = `[Alert][primary]
     This is a paragraph
     [/Alert]`;
 
-    expect(m2h.parse(md).html).toBe("<div class=\"alert alert-primary\" role=\"alert\">This is a paragraph</div>");
+    let expected = "<div class=\"alert alert-primary\" role=\"alert\">This is a paragraph</div>"
+
+    expect(m2h.parse(md).html).toBe(expected);
+});
+
+test("should return correct warning alert", async () => {
+
+    let md = `[Alert][warning]
+    This is a paragraph
+    [/Alert]`;
+
+    let expected = "<div class=\"alert alert-warning\" role=\"alert\">This is a paragraph</div>"
+
+    expect(m2h.parse(md).html).toBe(expected);
+});
+
+test("should return correct alert with highlight", async () => {
+
+    let md = `[Alert][warning]
+    This is a ==paragraph==
+    [/Alert]`;
+
+    let expected = "<div class=\"alert alert-warning\" role=\"alert\">This is a <mark>paragraph</mark></div>"
+
+    expect(m2h.parse(md).html).toBe(expected);
+});
+
+test("should return correct alert with html tags", async () => {
+
+    let md = `[Alert][warning]
+    <strong>bold</strong>
+    [/Alert]`;
+
+    let expected = "<div class=\"alert alert-warning\" role=\"alert\"><strong>bold</strong></div>"
+
+    expect(m2h.parse(md).html).toBe(expected);
 });
